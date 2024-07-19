b4b1dd3f031f8cac25466e90f980cf4aaa246fbc
Subham <bhandarimaiya65@gmail.com>
2024-07-19 18:17:16 +0545

    delete controller and functionality

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -76,6 +76,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        return 'destroy';
+        $note->delete();
+
+        return to_route('note.index')->with('message', 'Note was deleted successfully');
     }
 }
